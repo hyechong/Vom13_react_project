536d0a131decf87e14ea2f80b08ca903a5b17945
hyechong <ckgpwl2@gmail.com>
2023-08-08 20:32:03 +0900

    0808

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>감정일기장</h1>
+    </div>
+  );
 }
 
 export default App;
