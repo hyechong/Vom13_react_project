ce3715e0e984b2c2c0b10a013e37a1b00b296d8a
hyechong <ckgpwl2@gmail.com>
2023-08-09 18:04:07 +0900

    감정일기 작업중 0809

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,9 +1,108 @@
 import './App.css';
+import { getEmotionImgByID } from './util';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+import { useReducer, useRef, useEffect } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE': {
+      return [action.data, ...state];
+    }
+
+    case 'UPDATE': {
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? { ...action.data } : item
+      );
+    }
+
+    case 'DELETE': {
+      return state.filter(
+        (item) => String(item.id) !== String(action.targetId)
+      );
+    }
+
+    case 'INIT': {
+      return action.data;
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+const mockData = [
+  {
+    id: 'mock1',
+    date: new Date().getTime(),
+    content: 'mock1',
+    emotionId: 1,
+  },
+  {
+    id: 'mock2',
+    date: new Date().getTime(),
+    content: 'mock2',
+    emotionId: 2,
+  },
+  {
+    id: 'mock3',
+    date: new Date().getTime(),
+    content: 'mock3',
+    emotionId: 3,
+  },
+];
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    dispatch({
+      type: 'INIT',
+      data: mockData,
+    });
+  }, []);
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+    idRef.current += 1;
+  };
+  const onUpdate = (targetId, date, content, emotionId) => {
+    dispatch({
+      type: 'UPDATE',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  };
+  const onDelete = (targetId) => {
+    dispatch({
+      type: 'DELETE',
+      targetId,
+    });
+  };
   return (
     <div className='App'>
-      <h1>감정일기장</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='/edit' element={<Edit />} />
+      </Routes>
     </div>
   );
 }
