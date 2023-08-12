e28d8bfcb5f574bb53c37f7abe663c82e4b0d94d
hyechong <ckgpwl2@gmail.com>
2023-08-12 23:24:49 +0900

    수정하기 페이지 0812

--- diary/src/pages/New.js
+++ diary/src/pages/New.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DiaryDispatchContext } from '../App';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import Header from '../components/Header';
 
 const New = () => {
-  return <div>New페이지입니다</div>;
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate('/', { replace: true });
+  };
+  return (
+    <div>
+      <Header
+        title={'새 일기 쓰기'}
+        leftChild={<Button text={'< 뒤로가기'} onClick={goBack} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
